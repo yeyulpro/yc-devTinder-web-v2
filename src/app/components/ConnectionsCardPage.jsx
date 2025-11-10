58fde325e23c9038729c495c5facc9f7a0da8f18
yeyulpro <yeyulchoi2@gmail.com>
2025-11-09 23:13:43 -0500

    Add PaymentChoice UI component, update navbar item, and add chat button to ConnectionCard

--- src/app/components/ConnectionsCardPage.jsx
+++ src/app/components/ConnectionsCardPage.jsx
@@ -5,65 +5,16 @@ import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import CardActionArea from "@mui/material/CardActionArea";
 import { Link, useNavigate } from "react-router-dom";
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
 import Chip from "@mui/material/Chip";
 
 export default function ConnectionsCardPage({ user }) {
-  const {_id, first_name, last_name, photoUrl, about, age, gender } = user || {};
+  const { _id, first_name, last_name, photoUrl, about, age, gender } =
+    user || {};
 
   return (
-    // <Card
-    //   component={Paper}
-    //   elevation={11}
-    //   sx={{
-    //       width: 350,
-    //     height: 500,
-    //     boxShadow: 1,
-    //     bgcolor: "#1A2A4F",
-    //     borderRadius: 3,
-    //     border: "2px solid #0B192C",
-    //   }}
-    // >
-    //   <CardActionArea component={Link} to={`/user-profile/${_id}`}>
-    //     <CardMedia
-    //       component="img"
-    //       height="290"
-    //       image={photoUrl}
-    //       alt="user photo"
-    //       sx={{ p: 2, borderRadius: 7 }}
-    //     />
-    //     <CardContent>
-    //       <Typography
-    //         gutterBottom
-    //         variant="h5"
-    //         component="div"
-    //        sx={{ textTransform: "capitalize", color: "#FFFF" }}
-    //       >
-    //         {`${first_name} ${last_name}`}
-    //       </Typography>
-    //       <Typography variant="body2" color="#FFFF">
-    //         {age && gender && <>{age + ", " + gender}</>}
-    //       </Typography>
-    //       <Typography
-    //         variant="body2"
-    //         color="text.secondary"
-    //         sx={{
-    //            display: "-webkit-box",
-    //           WebkitLineClamp: 5,
-    //           WebkitBoxOrient: "vertical",
-    //           overflow: "hidden",
-    //           textOverflow: "ellipsis",
-    //           color: "#FFFF",
-    //         }}
-    //       >
-    //         {about}
-    //       </Typography>
-    //     </CardContent>
-    //   </CardActionArea>
-    // </Card>
-
-<Box sx={{ position: "relative", width: 350, height: 500 }}>
+    <Box sx={{ position: "relative", width: 350, height: 500 }}>
       <Card
         component={Paper}
         elevation={11}
@@ -113,9 +64,9 @@ export default function ConnectionsCardPage({ user }) {
             </Typography>
           </CardContent>
         </CardActionArea>
-
       </Card>
-      <IconButton color="secondary"
+      <IconButton
+        color="secondary"
         aria-label="chat"
         size="large"
         sx={{
@@ -123,12 +74,19 @@ export default function ConnectionsCardPage({ user }) {
           top: 280,
           right: 10,
         }}
-      onClick={()=>navigate('/chat')}>
-
-        <Chip label="Chat" variant="outlined" sx={{ color: '#F39F5A', fontWeight: 'bolder', "&:hover": { color: "#FFFF" }, fontSize: '1.1rem' }} />
+        onClick={() => navigate("/chat")}
+      >
+        <Chip
+          label="Chat"
+          variant="outlined"
+          sx={{
+            color: "#F39F5A",
+            fontWeight: "bolder",
+            "&:hover": { color: "#FFFF" },
+            fontSize: "1.1rem",
+          }}
+        />
       </IconButton>
     </Box>
-
-
   );
 }
